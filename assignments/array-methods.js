9d635cf373709106dd4621f55148222fe45a87b2
sage-jordan <sagemjrodan@gmail.com>
2019-05-24 21:52:02 -0400

    Attempting Challenge 5..

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -59,7 +59,7 @@ let fullName = [];
 runners.forEach((element, i) => {
     fullName.push(`${element.first_name} + ${element.last_name}`);
 });
-console.log(fullName);
+// console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
@@ -69,7 +69,7 @@ runners.map((name) =>  {
     allCaps.push(str.toUpperCase());
 });
 
-console.log(allCaps); 
+// console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
@@ -79,7 +79,7 @@ runners.filter((larges) => {
         largeShirts.push(larges);
     }
 });
-console.log(largeShirts);
+// console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
@@ -87,12 +87,27 @@ let ticketPriceTotal = [];
 runners.reduce((accumulator, currentValue) => {
     ticketPriceTotal.push(accumulator + currentValue.donation);
 }, 0);
-console.log(ticketPriceTotal);
+// console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// Username Generator! // Doesn't Work..
+let userGen = function(array, identification) {
+    let firstname = array[identification].first_name.toLowerCase();
+    let lastname = array[identification].last_name.toLowerCase();
+    let ID = array[identification].id;
+    let username = `${firstname}${lastname}${ID}`;
+    return username;
+}
+let generator = function(array){
+    array.forEach(() => {
+        console.log(userGen(array, this.id));
+    }, this);
+}
+console.log(generator(runners));
+
 
 // Problem 2
 
